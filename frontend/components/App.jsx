68c8de58b64a46c8294df876f679796589f25e58
Yehudah <jros987@gmail.com>
2021-01-22 11:51:54 -0500

    removed links

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -7,8 +7,7 @@ import signup_form_container from './session_handling/signup_form_container';
 const App = () => (
     <div>
         <h1> App.jsx rendering </h1>
-        <Link to='/login'> LogIn Form for now </Link>
-        <Link to='/signup'>SignUp Form for now</Link>
+        
         <Switch>
          <Route exact path="/login" component={login_form_container} />
         <Route exact path="/signup" component={signup_form_container} />  
